14a3771b0345eeb03844fa02fb8c2000e9c6023a
Stanley Azi <azistanley17@gmail.com>
2024-01-13 16:16:09 +0100

    new commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# iConvertUnits: a units converter app
+
+## What inspired the project?
+
+* To be able to convert units and copy the result with their respective suffix rather than just the number result then manually typing the suffix in my code. e.g '39px', '27rem'... rather than copy '27' then go back to my code to past it and then add the suffix 'rem'.
+* To have a nice looking UI. Since it a very important app I use often to convert units while coding, I need it to look very simple and classic.
+
+## What was considered during the building stage?
+
+* Well structured, reusable and readable functions.
+* Good regular expression to extract numbers only if user inputs lets say 100px, the number '100' is extracted and used for conversion. The regular expression also handles negative inputs, floating points numbers and of course integers.
+* Better UI to keep user happy while interacting with the app.
+* Time saving.
+* Keyboard accessibility for screen readers
+
+## Who can use this app?
+
+Anyone can use this app. The primary function of the app is to convert units, thus, anybody; Developers and Non-Developers can use this app for their unit conversion.
+
+## Is it open for contributions?
+
+Yes. This project is open for contributions. Feel free to add more units conversion functions, edit or correct any open issue or bug observed while using the app.
--- script.js
+++ script.js
@@ -1,29 +1,110 @@
-//px to rem converter
-const pxToRem = (px) => {
+//reusable PX to Relative Units (REM and EM) converter function using default 16px font-size
+const pxToRelativeUnit = (px) => {
     const pxString = String(px);
 
-    if (pxString.match(/\d+/)) {
-        const pattern = pxString.match(/\d+/);
+    if (pxString.match(/[-+]?\d*\.?\d+/)) {
+        const pattern = pxString.match(/[-+]?\d*\.?\d+/);
 
-        return `${Number(pattern[0]) / 16}px`;
-    } else {
-        throw Error('Not a valid number.');
-    }
+        const result = parseFloat(pattern[0]) / 16;
 
-}
-
-const remToPx = (rem) => {
-    const remString = String(rem);
-
-    if (remString.match(/\d+/)) {
-        const pattern = remString.match(/\d+/);
-
-        return `${Number(pattern[0]) * 16}rem`;
+        return Math.round(result * 10000) / 10000;
     } else {
         throw Error('Not a valid number.');
     }
 }
+//
 
-console.log(pxToRem('16'))
+//reusable function to convert relative units (REM and EM) to PX using default 16px font-size
+const relativeUnitToPx = (unit) => {
+    const remString = String(unit);
 
-console.log(remToPx('1'))
+    if (remString.match(/[-+]?\d*\.?\d+/)) {
+        const pattern = remString.match(/[-+]?\d*\.?\d+/);
+
+        const result = parseFloat(pattern[0]) * 16;
+
+        return Math.round(result * 10000) / 10000;
+    } else {
+        throw Error('Not a valid number.');
+    }
+};
+//
+//PX>>REM code block 
+const pxRemCodeBlock = () => {
+
+    //Convert PX to REM
+    const pxToRem = (pxValue) => `${pxToRelativeUnit(pxValue)}rem`;
+
+    //convert REM to PX
+    const remToPx = (remValue) => `${relativeUnitToPx(remValue)}px`;
+
+    //Get all inputs for px to rem converter
+    const pxInput = document.querySelector('.px_to_rem p #px');
+    const remInput = document.querySelector('.px_to_rem p #rem')
+
+    // An event listener to get px input current value
+    pxInput.addEventListener('input', () => {
+        const pxValue = pxInput.value;
+        
+        if (!pxValue) {
+            return remInput.value = ' ';
+        } else {
+            remInput.value = pxToRem(pxValue);
+        }
+    });
+    // 
+
+    // An event listener to get rem input current value
+    remInput.addEventListener('input', () => {
+        const remValue = remInput.value;
+        
+        if (!remValue) {
+            return pxInput.value = ' ';
+        } else {
+            pxInput.value = remToPx(remValue);
+        }
+    });
+    //
+}
+
+pxRemCodeBlock();
+//
+
+//PX>>Em code block 
+const pxEmCodeBlock = () => {
+    //Convert PX to EM
+    const pxToEm = (pxValue) => `${pxToRelativeUnit(pxValue)}em`;
+
+    //convert EM to PX
+    const emToPx = (emValue) => `${relativeUnitToPx(emValue)}px`;
+
+    //get inputs for px to em converter 
+    const pxInput = document.querySelector('.px_to_em p #px2');
+    const emInput = document.querySelector('.px_to_em p #em');
+
+    // An event listener to get px input current value
+    pxInput.addEventListener('input', () => {
+        const pxValue = pxInput.value;
+        
+        if (!pxValue) {
+            return emInput.value = ' ';
+        } else {
+            emInput.value = pxToEm(pxValue);
+        }
+    });
+    // 
+
+    // An event listener to get rem input current value
+    emInput.addEventListener('input', () => {
+        const emValue = emInput.value;
+        
+        if (!emValue) {
+            return pxInput.value = ' ';
+        } else {
+            pxInput.value = emToPx(emValue);
+        }
+    });
+}
+
+pxEmCodeBlock();
+//
